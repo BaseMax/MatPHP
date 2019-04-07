ebdd161738293fe94d7a327e199b523803ffe2ef
Max Base <MaxBaseCode@Gmail.Com>
2019-04-07 16:44:58 +0430

    Update MatPHP.php

--- MatPHP.php
+++ MatPHP.php
@@ -1,11 +1,11 @@
 <?php
 /**
 *
-* @Name : MTPHP.php
+* @Name : MatPHP.php
 * @Version : 1.0
 * @Programmer : Max
 * @Date : 2019-04-07
-* @Released under : https://github.com/BaseMax/MTPHP/blob/master/LICENSE
-* @Repository : https://github.com/BaseMax/MTPHP
+* @Released under : https://github.com/BaseMax/MatPHP/blob/master/LICENSE
+* @Repository : https://github.com/BaseMax/MatPHP
 *
 **/
